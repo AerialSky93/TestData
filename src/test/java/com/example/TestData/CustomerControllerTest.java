2a698129d307cb11cdd565400a12b583ee4e9e80
AerialSky93 <rdutta23@yahoo.com>
2023-11-11 11:39:01 -0800

    add mock for publisher

--- src/test/java/com/example/TestData/CustomerControllerTest.java
+++ src/test/java/com/example/TestData/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.TestData;
 
 import com.example.TestData.controller.CustomerController;
+import com.example.TestData.event.CustomerPublisher;
 import com.example.TestData.repository.CustomerRepository;
 import com.example.TestData.response.CustomerGetResponse;
 import org.junit.jupiter.api.Test;
@@ -17,9 +18,10 @@ class CustomerControllerTest {
     @Test
     void getCustomerIdReturnsResults() {
         CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
+        CustomerPublisher customerPublisher = Mockito.mock(CustomerPublisher.class);
         CustomerGetResponse expectedCustomerGetResponse = new CustomerGetResponse();
         when(customerRepository.findByCustomerId(1)).thenReturn(expectedCustomerGetResponse);
-        CustomerController customerController = new CustomerController(customerRepository);
+        CustomerController customerController = new CustomerController(customerRepository, customerPublisher);
 
         //MockHttpServletResponse response = new MockHttpServletResponse();
         CustomerGetResponse actualCustomerGetResponse = customerController.getCustomerById(1);
