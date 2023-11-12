79b96aabdf1fe63c7a5fe9159d144a9178e309b9
AerialSky93 <rdutta23@yahoo.com>
2023-11-12 14:06:45 -0800

    add server configuration changes

--- src/main/java/com/example/TestData/config/ServerConfiguration.java
+++ src/main/java/com/example/TestData/config/ServerConfiguration.java
@@ -1,28 +1,41 @@
 package com.example.TestData.config;
 
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ServerConfiguration {
 
-    // Passing the key which you set in application.properties
     @Value("${apiKey}")
     private String apiKey;
-
-
-    // Getting value key which are  set in application.properties
     @Bean
     public String getApiKey() {
         return apiKey;
     }
 
+    @Bean
+    public DataSource getDataSource() {
+        SQLServerDataSource ds = new SQLServerDataSource();
 
-/*    @Bean
+        //ds.setServerName("test-data-server.database.windows.net");
+        ds.setServerName("localhost");
+        ds.setUser("sauser");
+        ds.setPassword("sapwd12345*");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        ds.setDatabaseName("test");
+        return ds;
+    }
+
+    @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
-        return new NamedParameterJdbcTemplate(new DataSource());
-    }*/
+        return new NamedParameterJdbcTemplate(getDataSource());
+    }
 }
